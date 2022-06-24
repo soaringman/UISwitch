a763b777bce4b69850957296033d6fe4da47b1fc
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-24 17:16:38 +0300

    Initial Commit

---
+++ UISwitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISwitch
+//
+//  Created by Алексей Гуляев on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
