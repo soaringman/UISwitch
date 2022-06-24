f94fed3500b48d979cfb699b8dd5282f42835f8f
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-24 17:58:45 +0300

    1. Add UISwitch by code 2. Add observing by code

--- UISwitch/ViewController.swift
+++ UISwitch/ViewController.swift
@@ -5,13 +5,51 @@
 //  Created by Алексей Гуляев on 24.06.2022.
 //
 
+/*
+ Create UISwitch by code
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let mySwitch = UISwitch()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        //Create switch with frame
+        self.mySwitch.frame = CGRect(x: 100,
+                                     y: 100,
+                                     width: 0,
+                                     height: 0)
+        self.view.addSubview(self.mySwitch)
+        self.mySwitch.setOn(true, animated: true)
+        
+        //Check switch (off or on)
+        if self.mySwitch.isOn {
+            print("swith is on")
+        } else {
+            print("swith is off")
+        }
+        
+        //Add observing
+        self.mySwitch.addTarget(self,
+                                action: #selector(switchChange(paramTarget:)),
+                                for: .valueChanged)
+    }
+    
+    //Add method for #selector
+    @objc func switchChange(paramTarget: UISwitch) {
+        
+        print("parametr is = ", paramTarget)
+        
+        if paramTarget.isOn {
+            print("swith is on")
+        } else {
+            print("swith is off")
+        }
+        
     }
 
 
